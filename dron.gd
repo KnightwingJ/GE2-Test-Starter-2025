5133b307404c89c03e36a19af6382d4565634f76
John Hinch <johnhnch864@gmail.com>
2025-04-29 10:59:35 +0100

    rotate not leftright

--- dron.gd
+++ dron.gd
@@ -8,9 +8,9 @@ func _process(delta: float) -> void:
 	if Input.is_key_pressed(KEY_S):
 		position=position+global_transform.basis.z *0.003
 	if Input.is_key_pressed(KEY_A):
-		position=position-global_transform.basis.x *0.003
+		global_rotate(Vector3.UP,0.05)
 	if Input.is_key_pressed(KEY_D):
-		position=position+global_transform.basis.x *0.003
+		global_rotate(Vector3.DOWN,0.05)
 	if Input.is_action_just_released("up"):
 		position=position - global_transform.basis.y * 0.003
 	if Input.is_key_pressed(KEY_O):
